a8a6b1372a688c9f7f2644c99a65790eb59f4620
Duck-sot <raja07001@utb.vaxjo.se>
2024-12-13 13:58:55 +0100

    carti

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿int a = Convert.ToInt32(Console.ReadLine);
+﻿/*
+int a = Convert.ToInt32(Console.ReadLine());
 if (a<0){
     Console.WriteLine("negative");
 }
@@ -7,4 +8,62 @@ if (a == 0){
 }
 if ( a > 0 ){
     Console.WriteLine("posetive");
+}
+
+List<string> vege = new List<string>();
+vege.Add("tomato");
+vege.Add("apple");
+vege.Add("wattermellon");
+vege.Add("mango");
+
+foreach (string element in vege){
+    Console.WriteLine(element);
+}
+
+C c = new C();
+int i = 0;
+Add(c);
+AddInt(ref i);
+
+Console.WriteLine(c.num);
+Console.WriteLine(i);
+
+void Add(C c){
+    c.num++;
+}
+
+void AddInt(ref int num){
+    num++;
+}
+class C {
+    public int num = 2;
+}
+*/
+
+
+
+class Car {
+
+    private string model; 
+    private string brand; 
+    private int årsmodel; 
+    public string Model{
+        set{model = value;}
+        get{return model;}
+    }
+    public string Brand{
+        set{brand = value;}
+        get{return brand;}
+    }
+    public int Årsmodel{
+        set{ årsmodel=value;}
+        get{return årsmodel;}
+    }
+    public Car(string model, string brand, int årsmodel){
+        Console.WriteLine(model);
+        
+    }
+
+
+
 }
